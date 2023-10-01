77c8487df5e9e29d014f52f9279ed7ea86f84251
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-01 05:11:49 +0100

    question 112 in leetcode

---
+++ python/112.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+
+        if not root.right and not root.left:
+            return root.val == targetSum
+
+        return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
