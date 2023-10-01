31e69cc1c59d525cae68589690034f3eeb2a9308
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-02 00:27:59 +0100

    question 2626 in leetcode

---
+++ javascript/2626.js
@@ -0,0 +1,8 @@
+var reduce = function (nums, fn, init) {
+    if (nums.length == 0) return init;
+    let val = fn(init, nums[0]);
+    for (let i = 1; i < nums.length; i++) {
+        val = fn(val, nums[i]);
+    }
+    return val;
+};
