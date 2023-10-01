68b9f5ccd7b4ebcf83a876d754acf9e4ea84132d
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-02 00:28:17 +0100

    question 2236 in leetcode

---
+++ python/2236.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        if root == None:
+            return False
+        return root.left.val + root.right.val == root.val
