b475f33311a0b983073f8774d57c1b7e681dfcc9
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-11 13:28:08 +0100

    question 2009 in leetcode

---
+++ python/2009.py
@@ -0,0 +1,17 @@
+from bisect import bisect_right
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = n
+        nums = sorted(set(nums))
+
+        for i, start in enumerate(nums):
+            end = start + n - 1
+            index = bisect_right(nums, end)
+            uniqueLength = index - i
+            ans = min(ans, n - uniqueLength)
+
+        return ans
