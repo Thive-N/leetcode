f21e61301974c98064ab5f41b73a81a95c15a6c7
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-13 00:19:43 +0100

    question 226 in leetcode

--- python/226.py
+++ python/226.py
@@ -1,26 +1,10 @@
-from typing import List
-
-
+# Definition for a binary tree node.
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
-        ans1 = 0
-        ans2 = 1
-        count1 = 0
-        count2 = 0
-
-        for num in nums:
-            if num == ans1:
-                count1 += 1
-            elif num == ans2:
-                count2 += 1
-            elif count1 == 0:
-                ans1 = num
-                count1 = 1
-            elif count2 == 0:
-                ans2 = num
-                count2 = 1
-            else:
-                count1 -= 1
-                count2 -= 1
-
-        return [ans for ans in (ans1, ans2) if nums.count(ans) > len(nums) // 3]
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
