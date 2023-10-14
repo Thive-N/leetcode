1410aa480c4a068c36a041fb649544ea716b2d20
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-14 17:14:39 +0100

    question 226 in leetcode

--- python/226.py
+++ python/226.py
@@ -1,10 +1,21 @@
 # Definition for a binary tree node.
 from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
+        if root == None:
+            return root
+
+        l = root.left
+        r = root.right
+        root.left = self.invertTree(r)
+        root.right = self.invertTree(l)
+        return root
