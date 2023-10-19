8df750cc9424911c4259d3331a5835ccbf0ea6a0
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-19 20:15:00 +0100

    question 376 in leetcode
    difficulty: medium

---
+++ python/376.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        inc = 1
+        dec = 1
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                inc = dec+1
+            elif nums[i] < nums[i-1]:
+                dec = inc+1
+        return max(inc, dec)
