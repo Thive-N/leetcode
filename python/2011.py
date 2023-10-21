8e44c51322c462c4f86102924ee21518cf308fae
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-21 20:34:12 +0100

    question 2011 in leetcode
    difficulty: easy

---
+++ python/2011.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for op in operations:
+            if op in ["++X", "X++"]:
+                x += 1
+            else:
+                x -= 1
+        return x
