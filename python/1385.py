b906c71c88a3572177dbfcfb5c660eb1fe2999ca
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-26 15:38:07 +0100

    question 1385 in leetcode
    difficulty: easy

---
+++ python/1385.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+        return sum([all([abs(a - b) > d for b in arr2]) for a in arr1])
