ed393dcb67ba5de214c671e935c2612882653f34
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-31 13:10:45 +0000

    question 2287 in leetcode
    difficulty: easy

---
+++ python/2287.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+
+class Solution:
+    def rearrangeCharacters(self, s: str, target: str) -> int:
+        countS = Counter(s)
+        countT = Counter(target)
+        return min(countS[c] // countT[c] for c in target)
