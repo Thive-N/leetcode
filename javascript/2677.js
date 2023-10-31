63b8bbbd141dc4a8af5df6218e1cd700f1e10a3a
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-31 13:05:21 +0000

    question 2677 in leetcode
    difficulty: easy

---
+++ javascript/2677.js
@@ -0,0 +1,7 @@
+var chunk = function (arr, size) {
+    const ans = [];
+    for (let i = 0, n = arr.length; i < n; i += size) {
+        ans.push(arr.slice(i, i + size));
+    }
+    return ans;
+};
