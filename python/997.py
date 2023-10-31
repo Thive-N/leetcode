a5107497c4488b040b9ddeb0cb0cc5179e2132f1
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-31 13:07:31 +0000

    question 997 in leetcode
    difficulty: easy

---
+++ python/997.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        count = [0 for _ in range(n + 1)]
+
+        for a, b in trust:
+            count[a] -= 1
+            count[b] += 1
+
+        for i in range(1, n + 1):
+            if count[i] == n - 1:
+                return i
+
+        return -1
