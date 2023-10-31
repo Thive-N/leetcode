e42647586262db2bf22549fee9d19c5eaa7df59c
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-31 13:10:35 +0000

    question 2465 in leetcode
    difficulty: easy

---
+++ python/2465.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def distinctAverages(self, nums: List[int]) -> int:
+        n = len(nums)
+        sums = set()
+        nums.sort()
+        for i in range(n // 2):
+            sums.add(nums[i] + nums[n - i - 1])
+        return len(sums)
