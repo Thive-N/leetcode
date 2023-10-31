5c7a30742135035e8efd7739e41e86560b9cac0d
Thive-N <35178093+Thive-N@users.noreply.github.com>
2023-10-31 13:10:55 +0000

    question 2154 in leetcode
    difficulty: easy

---
+++ python/2154.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        while original in nums:
+            original *= 2
+
+        return original
