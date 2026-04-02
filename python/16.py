0708512802c26791e36b780d4d8aa6d9324ce182
Thive-N <35178093+Thive-N@users.noreply.github.com>
2026-04-02 01:00:20 +0100

    question 16 difficult: medium

---
+++ python/16.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        result = sum(nums[:3])
+        
+        for i in range(len(nums) - 2):
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if abs(target - total) < abs(target - result):
+                    result = total
+
+                if total == target:
+                    return target
+                elif total < target:
+                    left += 1
+                else:
+                    right -= 1
+
+        return result
