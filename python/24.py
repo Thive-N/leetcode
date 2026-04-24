8b6b70ac0ee698e37383763c6d8e2f69a7a68a29
Thive-N <35178093+Thive-N@users.noreply.github.com>
2026-04-24 19:22:21 +0100

    question 24

---
+++ python/24.py
@@ -0,0 +1,18 @@
+def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+	if not head:
+        return head
+
+	prev = None
+    cur = head
+    ans = head.next
+	while cur and cur.next:
+		adj = cur.next
+		if prev:
+            prev.next = adj
+
+		cur.next = adj.next
+        adj.next = cur
+		prev = cur
+        cur = cur.next
+
+	return ans or head
