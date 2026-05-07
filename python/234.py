8f820ddefd0e7ecdb9b2026cc979b0d1e179f577
Thive-N <35178093+Thive-N@users.noreply.github.com>
2026-05-08 00:35:38 +0100

    question 234

---
+++ python/234.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        list_vals = []
+        while head:
+            list_vals.append(head.val)
+            head = head.next
+        
+        left, right = 0, len(list_vals) - 1
+        while left < right and list_vals[left] == list_vals[right]:
+            left += 1
+            right -= 1
+        return left >= right
