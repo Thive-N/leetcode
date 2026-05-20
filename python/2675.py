5c092452a00d8587d965e710a8c7e3ef96d34ce2
Thive-N <35178093+Thive-N@users.noreply.github.com>
2026-05-20 21:41:25 +0100

    question 2675

---
+++ python/2675.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        n = len(A)
+        res = [0] * n
+        seen = set()
+        
+        for i in range(n):
+            seen[0] += seen[A[i]]
+            seen[A[i]] = 1
+            
+            seen[0] += seen[B[i]]
+            seen[B[i]] = 1
+            
+            res[i] = seen[0]
+            
+        return res
