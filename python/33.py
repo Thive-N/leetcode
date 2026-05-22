2828e1a499b30b0e909b677e341a0fc4a4de8000
Thive-N <35178093+Thive-N@users.noreply.github.com>
2026-05-22 22:47:12 +0100

    question 33

---
+++ python/33.py
@@ -0,0 +1,32 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+
+        # (smallest element index)
+        left, right = 0, n - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+
+        rotation_index = left
+
+        # normal binary search
+        left, right = 0, n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            # map mid into rotated array
+            real_mid = (mid + rotation_index) % n
+
+            if nums[real_mid] == target:
+                return real_mid
+            elif nums[real_mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
