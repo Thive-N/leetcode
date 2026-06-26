f23463f57ed6bac197cf3b0a49872f134ebd5697
Thive-N <35178093+Thive-N@users.noreply.github.com>
2026-06-26 18:00:52 +0100

    question 100

--- python/100.py
+++ python/100.py
@@ -1,27 +1,22 @@
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
-
-class Solution(object):
-    def isSameTree(self, p, q):
-        self.a = p
-        self.b = q
-        return self.check(self.a, self.b)
-
-    def check(self, a, b):
-        if a is None and b is None:
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p is None and q is None:
             return True
 
-        elif a is None and b is not None:
+        if p is None or q is None:
             return False
 
-        elif a is not None and b is None:
+        if p.val != q.val:
             return False
 
-        elif a.val != b.val:
-            return False
-        else:
-            return self.check(a.left, b.left) and self.check(a.right, b.right)
+        return (
+            self.isSameTree(p.left, q.left) and
+            self.isSameTree(p.right, q.right)
+        )
