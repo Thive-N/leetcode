4b79da56c66c65bf0e4158ad87f335c4f21e4112
Thive-N <35178093+Thive-N@users.noreply.github.com>
2026-07-13 22:36:10 +0100

    question 1291

---
+++ python/1291.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> list[int]:
+        res = []
+
+        for i in range(1, 10):
+            num = i
+            for j in range(i + 1, 10):
+                num = num * 10 + j
+                if low <= num <= high:
+                    res.append(num)
+
+        res.sort()
+        return res
