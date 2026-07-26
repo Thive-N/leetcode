427b9b03e248f586486e9684c987d677b8e8282d
Thive-N <35178093+Thive-N@users.noreply.github.com>
2026-07-26 13:50:55 +0100

    question 1448

--- python/1448.py
+++ python/1448.py
@@ -5,19 +5,19 @@ class TreeNode:
         self.left = left
         self.right = right
 
-      
+
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         def dfs(node, max_val):
             if not node:
                 return 0
-            
+
             count = 1 if node.val >= max_val else 0
             max_val = max(max_val, node.val)
-            
+
             count += dfs(node.left, max_val)
             count += dfs(node.right, max_val)
-            
+
             return count
-        
+
         return dfs(root, root.val)
