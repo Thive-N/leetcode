30563eb0fb99a2c9bb90a14ce4cb7a8c64cc8a2c
Thive-N <35178093+Thive-N@users.noreply.github.com>
2026-08-30 18:12:06 +0100

    question 3658

--- python/3658.py
+++ python/3658.py
@@ -1,11 +1,3 @@
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
-        odd_sum = sum(i for i in range(1, n + 1) if i % 2 != 0)
-        even_sum = sum(i for i in range(1, n + 1) if i % 2 == 0)
-        
-        def gcd(a: int, b: int) -> int:
-            while b:
-                a, b = b, a % b
-            return a
-        
-        return gcd(odd_sum, even_sum)
+        return n
